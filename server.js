c6498aab6ec5cea5be2dd5bb1abbc8f08b4a35a9
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-22 18:12:09 -0500

    created route files to handle URLs, will edit API routes to reflect posted data. modeled after HotRestaurant activity

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const noteDatabase = ('./db/db.json');
 
 const app = express();
 
 const PORT = process.env.PORT || 8675;
 
+let notes = [];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+
+require('./routes/htmlRoutes')(app);
+// require('./routes/apiRoutes')(app);
 
 
 
