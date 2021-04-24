2a8b83cff2c17cc3832a2ddfbc27321fb43f4ab4
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-24 00:23:21 -0500

    Attempting to have api/notes page load with json to no success, have tried multiple routes and styles of editing the code..

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,31 @@
 const noteDatabase = require('../db/db.json');
+// const noteDatabase = require('../db/notes');
 const fs = require('fs');
-// const generateUniqueId = require('generate-unique-id');
-
-
-
-// const id = generateUniqueId({
-//     length: 2,
-//     useLetters: false
-//    });
+const generateUniqueId = require('generate-unique-id');
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteDatabase));
+    app.get('/api/notes', (req, res) => {
+    res.json(noteDatabase);
+    console.log(noteDatabase);
+});
 
 
-    // const updateNoteDatabase = () => {
-    //     fs.writeFile('db.json', JSON.stringify(noteDatabase), err => {
-    //         if (err) throw err;
-    //         return true;
-    //     });
-    // };
+    const updateNoteDatabase = () => {
+        
+        fs.writeFile('./db/db.json', JSON.stringify(noteDatabase), err => {
+            if (err) throw err;
+            return true;
+        });
+    };
 
 
     app.post('/api/notes', (req, res) => {
         const newNotePost = req.body
-        // newNotePost.id = id();
+        const id = generateUniqueId();
+        newNotePost.id = id;
         console.log(newNotePost);
         noteDatabase.push(newNotePost);
         res.json(true);
-        // updateNoteDatabase()
+        updateNoteDatabase();
     });
 };
