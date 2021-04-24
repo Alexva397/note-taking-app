3a59489099e1efb2d5e1a75aebc35c0f3a33ad09
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-24 18:05:31 -0500

    successfully moved api routing to seperate file with no issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,47 @@
-// const fs = require('fs');
-// const path = require('path');
-// const noteDatabase = require('../db/db.json');
-// const generateUniqueId = require('generate-unique-id');
+// dependencies
+const fs = require('fs');
+const noteDatabase = require('../db/db.json');
+const generateUniqueId = require('generate-unique-id');
 
+// Assign an array to JSON file
+let notesArray = noteDatabase;
 
+module.exports = app => {
+    app.get('/api/notes', (req, res) => {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+          if (err) throw err;
+          res.json(JSON.parse(data));
+        })
+      });
+      
+      app.post('/api/notes', (req, res) => {
+        const newNotePost = req.body;
+        const newId = generateUniqueId();
+        newNotePost.id = newId;
+        notesArray.push(newNotePost);
+      
+        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err) =>
+          err ? console.log(err) : console.log(`successfully saved note: ${newNotePost.id}`));
+        res.json(newNotePost);
+      });
+      
+      app.get('api/notes/:id', (req, res) => {
+        const userCoice = req.params.id;
+        res.json(userCoice);
+      });
+      
+      
+      app.delete('/api/notes/:id', (req, res) => {
+        const deleteChoice = req.params.id;
+        for (i = 0; i < notesArray.length; i++) {
+          if (deleteChoice === notesArray[i].id) {
+            notesArray.splice(i, 1);
+          }
+        }
+        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err) =>
+          err ? console.log(err) : console.log('success'));
+        res.json(notesArray);
+        console.log(`Note ${deleteChoice} successfully deleted!`);
+      });
 
-// module.exports = app => {
-//     app.get('/api/notes', (req, res) => res.json(noteDatabase));
-//     //     fs.readFile('../db/db.json', 'utf8', (err, data) => {
-//     //         if (err) throw err;
-//     //         notes = JSON.parse(data);
-//     //         return notes;
-//     //     })
-//     //     res.json(notes);
-//     // });
-
-    
-
-//     const updateNoteDatabase = () => {
-
-//         fs.writeFile('db/db.json', JSON.stringify(notes), err => {
-//             if (err) throw err;
-//             return true;
-//         });
-//     };
-
-
-//     app.post('/api/notes', (req, res) => {
-//         const newNotePost = req.body
-//         const newId = generateUniqueId();
-//         newNotePost.id = newId;
-//         console.log(newNotePost);
-//         notes.push(newNotePost);
-//         res.json(newNotePost);
-//         updateNoteDatabase();
-//     });
-
-// };
+};
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const noteDatabase = require('./db/db.json');
-const fs = require('fs');
+// const noteDatabase = require('./db/db.json');
+// const fs = require('fs');
 const path = require('path');
-const generateUniqueId = require('generate-unique-id');
+// const generateUniqueId = require('generate-unique-id');
 
 const app = express();
 const PORT = process.env.PORT || 8675;
@@ -12,52 +12,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // require('./routes/htmlRoutes')(app);
-// require('./routes/apiRoutes')(app);
-
-// Assign an array to JSON file
-let notesArray = noteDatabase;
-
+require('./routes/apiRoutes')(app);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-
-app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    res.json(JSON.parse(data));
-  })
-});
-
-app.post('/api/notes', (req, res) => {
-  const newNotePost = req.body;
-  const newId = generateUniqueId();
-  newNotePost.id = newId;
-  notesArray.push(newNotePost);
-
-  fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err) =>
-    err ? console.log(err) : console.log(`successfully saved note: ${newNotePost.id}`));
-  res.json(newNotePost);
-});
-
-app.get('api/notes/:id', (req, res) => {
-  const userCoice = req.params.id;
-  res.json(userCoice);
-});
-
-
-app.delete('/api/notes/:id', (req, res) => {
-  const deleteChoice = req.params.id;
-  for (i = 0; i < notesArray.length; i++) {
-    if (deleteChoice === notesArray[i].id) {
-      notesArray.splice(i, 1);
-    }
-  }
-  fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err) =>
-    err ? console.log(err) : console.log('success'));
-  res.json(notesArray);
-  console.log(`Note ${deleteChoice} successfully deleted!`);
-});
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
