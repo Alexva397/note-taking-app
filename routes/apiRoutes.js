33e176bbde382b27019d54130eff4c0476fcbde0
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-24 16:49:43 -0500

    cannot get route files to work correctly, rewritten get and post code in server.js, as well as html paths

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,39 @@
-// const noteDatabase = require('../db/db');
-// const noteDatabase = require('../db/notes');
-const fs = require('fs');
-const generateUniqueId = require('generate-unique-id');
-
-let notesArray = [];
-
-module.exports = (app) => {
-    fs.readFile('db/db.json', 'utf8', (err, notes) => {
-        if (err) throw err;
-
-        let notesArray = JSON.parse(notes);
-
-        app.get('/api/notes', (req, res) => res.json(notesArray));
+// const fs = require('fs');
+// const path = require('path');
+// const noteDatabase = require('../db/db.json');
+// const generateUniqueId = require('generate-unique-id');
 
 
 
+// module.exports = app => {
+//     app.get('/api/notes', (req, res) => res.json(noteDatabase));
+//     //     fs.readFile('../db/db.json', 'utf8', (err, data) => {
+//     //         if (err) throw err;
+//     //         notes = JSON.parse(data);
+//     //         return notes;
+//     //     })
+//     //     res.json(notes);
+//     // });
 
-        const updateNoteDatabase = () => {
+    
 
-            fs.writeFile('db/db.json', JSON.stringify(notesArray), err => {
-                if (err) throw err;
-                return true;
-            });
-        };
+//     const updateNoteDatabase = () => {
+
+//         fs.writeFile('db/db.json', JSON.stringify(notes), err => {
+//             if (err) throw err;
+//             return true;
+//         });
+//     };
 
 
-        app.post('/api/notes', (req, res) => {
-            const newNotePost = req.body
-            const newId = generateUniqueId();
-            newNotePost.id = newId;
-            console.log(newNotePost);
-            notesArray.push(newNotePost);
-            res.json(newNotePost);
-            updateNoteDatabase();
-        });
-    })
-};
+//     app.post('/api/notes', (req, res) => {
+//         const newNotePost = req.body
+//         const newId = generateUniqueId();
+//         newNotePost.id = newId;
+//         console.log(newNotePost);
+//         notes.push(newNotePost);
+//         res.json(newNotePost);
+//         updateNoteDatabase();
+//     });
+
+// };
--- server.js
+++ server.js
@@ -1,23 +1,44 @@
 const express = require('express');
+const noteDatabase = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
+const generateUniqueId = require('generate-unique-id');
 
 const app = express();
 
 const PORT = process.env.PORT || 8675;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 app.use(express.static('public'));
 
-require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
-// const apiRoutes = require('./routes/apiRoutes');
-// app.use('./api', apiRoutes);
+// require('./routes/htmlRoutes')(app);
+// require('./routes/apiRoutes')(app);
+
+// Assign an array to JSON file
+let notesArray = noteDatabase;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  })
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNotePost = req.body;
+  const newId = generateUniqueId();
+  newNotePost.id = newId;
+  notesArray.push(newNotePost);
+
+  fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err) =>
+  err ? console.log(err) : console.log('successfully saved note.'));
+  res.json(newNotePost);
+});
 
 
 app.listen(PORT, () => {
